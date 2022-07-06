0a802f9a9baf7a2c435b57e1c36a6bdde720ee4d
Filip Pawlak <filippawlak@gmail.com>
2022-07-06 15:29:42 +0200

    Add some HTTP routes

--- src/main/scala/com/evolution/projekt/Server.scala
+++ src/main/scala/com/evolution/projekt/Server.scala
@@ -23,7 +23,50 @@ import io.circe.generic.JsonCodec
 import io.circe.parser._
 import io.circe.syntax._
 
+import java.time.LocalDate
+import scala.util.Try
+
 object Server extends IOApp {
+  private val serverRoutes = {
+    object LocalDateVar {
+      def unapply(value: String): Option[LocalDate] =
+        Try(LocalDate.parse(value)).toOption
+    }
+
+    object GradeVar {
+      def unapply(value: String): Option[Int] =
+        Try(value.toInt).toOption.flatMap(i =>
+          if(0 to 5 contains i) Some(i) else None)
+    }
+
+
+    HttpRoutes.of[IO] {
+      // all cards
+      // curl "localhost:9001/cards"
+      case GET -> Root / "cards" => Ok("hello")
+
+      // cards for a certain date
+      // curl "localhost:9001/hello/world"
+      case GET -> Root / "cards" / LocalDateVar(localDate) => Ok("hello")
+
+      // adding a card
+      // curl -XPOST "localhost:9001/addCard"
+      case req @ POST -> Root / "addCard" => Ok("hello")
+
+      // grading a card
+      // curl -XPOST "localhost:9001/grade/100/5"
+      case POST -> Root / "grade" / IntVar(cardId) / GradeVar(grade) => Ok("hello")
+    }
+  }
+
+  val httpApp = serverRoutes.orNotFound
+
   override def run(args: List[String]): IO[ExitCode] =
-    IO(println("Hello from the server!")).as(ExitCode.Success)
+    BlazeServerBuilder[IO](ExecutionContext.global)
+      .bindHttp(port = 9001, host = "localhost")
+      .withHttpApp(httpApp)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
