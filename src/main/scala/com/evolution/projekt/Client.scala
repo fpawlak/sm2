dd7788820d7a9e9b07b355bec40e13eedb1cdaa2
Filip Pawlak <filippawlak@gmail.com>
2022-07-11 17:49:31 +0200

    Action selection menu

--- src/main/scala/com/evolution/projekt/Client.scala
+++ src/main/scala/com/evolution/projekt/Client.scala
@@ -4,10 +4,37 @@ import cats._
 import cats.data._
 import cats.effect._
 import cats.implicits._
+import scala.util.{Try,Success,Failure}
 
 object Client extends IOApp {
 
-  override def run(args: List[String]): IO[ExitCode] = {
-    IO(println("Hello from the client!")).as(ExitCode.Success)
+  object Console {
+    def putStrLn(value: String): IO[Unit] = IO(println(value))
+    val readLn: IO[String] = IO(scala.io.StdIn.readLine())
   }
+
+  import Console._
+
+  val selectAction: IO[Int] = for {
+    _ <- putStrLn("Select action:")
+    _ <- putStrLn("1 - List all cards by date")
+    _ <- putStrLn("2 - Review cards scheduled for today")
+    _ <- putStrLn("3 - Review cards scheduled for a certain date")
+    _ <- putStrLn("4 - Add a new card")
+    selectionStr <- readLn
+    selection <- {
+      val retry: IO[Int] = putStrLn("Wrong input!") >> selectAction
+      Try(selectionStr.toInt) match {
+        case Failure(_) => retry
+        case Success(selectionInt) =>
+          if(1 to 4 contains selectionInt) IO.pure(selectionInt)
+          else retry
+      }
+    }
+  } yield selection
+
+  override def run(args: List[String]): IO[ExitCode] = for {
+    selection <- selectAction
+    _ <- putStrLn(s"Your selection: $selection")
+  } yield ExitCode.Success
 }
